7f42cafb378e51a43ced8e394124d1249702cae5
murphy <87026989+m-ur-phy@users.noreply.github.com>
2022-04-21 00:05:53 +0900

    Increment and decrement operators... 너무 졸려서 여기까지만...

--- src/practice.js
+++ src/practice.js
@@ -8,3 +8,27 @@ console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
+
+// 연결 연산자
+let c = "1";
+let d = 2; // 묵시적 형변환
+
+console.log(c + d);
+
+// 복합 연산자
+let e = 5;
+
+// e = e + 10;
+e += 10;
+e -= 10;
+e *= 2;
+e /= 2;
+
+console.log(e);
+
+// 증감 연산자 (숫자형에만 사용 가능)
+let f = 10;
+
+f++;
+
+console.log(f);
